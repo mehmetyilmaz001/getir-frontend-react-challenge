b153f6ef394787ee14d59acf4bd522dd962d6e8a
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2021-12-27 02:30:42 +0300

    Added production api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:4000';
+import { isProd } from './../helpers/common';
+export const API_BASE_URL = isProd ? 'https://mock-product-service.herokuapp.com' : 'http://localhost:4000';
 export const API_ITEMS_URL = `${API_BASE_URL}/items`;
 export const API_TAGS_URL = `${API_BASE_URL}/tags`;
 export const API_BRANDS_URL = `${API_BASE_URL}/brands`;
